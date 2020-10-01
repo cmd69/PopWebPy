cfede830ee2c80b46d78e884020cc2b7b1499059
nasamigra <nasamigra@gmail.com>
2020-10-01 13:50:53 +0200

    images added login logout working

--- PopWebPy/settings.py
+++ PopWebPy/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shop_db',
         'USER': 'postgres',
-        'PASSWORD': 'contraseña',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'DATABASE_PORT': '5432',
     }
